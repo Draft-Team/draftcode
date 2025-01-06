e64e5c0939120db20bf1dccad85c9d8da01126ed
unknown <frete497@gmail.com>
2025-01-06 02:48:55 -0400

    refactor: change profile structure

---
+++ app/features/profile/pages/profile-edit/profile-edit-page.tsx
@@ -0,0 +1,143 @@
+import { Link } from "@tanstack/react-router"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { ArrowLeft, Globe } from "lucide-react"
+import { useForm } from "react-hook-form"
+
+import { cn } from "@/libs/utils"
+import { Button, buttonVariants } from "@/shared/components/ui/button"
+import { Input } from "@/shared/components/ui/input"
+import { Label } from "@/shared/components/ui/label"
+import { Textarea } from "@/shared/components/ui/textarea"
+
+import { useProfile } from "../../hooks/use-profile"
+import { ProfileSchema, type ProfileData } from "../../schemas/profile-schema"
+import { useEditProfile } from "./hooks/use-edit-profile"
+
+export const ProfileEditPage = () => {
+	const { profile } = useProfile()
+	const { mutate: edit, isPending } = useEditProfile()
+
+	const links = profile?.links.reduce<Record<string, string>>((acc, item) => {
+		acc[item.type] = item.url
+		return acc
+	}, {})
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<ProfileData>({
+		resolver: zodResolver(ProfileSchema),
+		defaultValues: {
+			bio: profile?.bio ?? "",
+			githubUrl: links?.github ?? "",
+			linkedinUrl: links?.linkedin ?? "",
+			twitchUrl: links?.twitch ?? "",
+			youtubeUrl: links?.youtube ?? "",
+			websiteUrl: links?.website ?? ""
+		}
+	})
+
+	const onSubmit = (data: ProfileData): void => {
+		edit({ data })
+	}
+
+	return (
+		<main className="container">
+			<Link
+				to="/profile"
+				className={cn(buttonVariants({ variant: "outline" }), "font-medium")}>
+				<ArrowLeft /> Voltar ao perfil
+			</Link>
+
+			<form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+				<div className="mb-4 flex items-center justify-between">
+					<h1 className="text-2xl font-semibold">Editar Perfil</h1>
+					<Button type="submit" mode="loading" className="mb-4" isLoading={isPending}>
+						Salvar
+					</Button>
+				</div>
+
+				<div className="grid grid-cols-1 gap-4">
+					<div className="flex h-max flex-col gap-4 border bg-card p-4">
+						<fieldset className="flex flex-col gap-4">
+							<Label className="space-y-2" htmlFor="bio">
+								<span>Bio</span>
+								<Textarea {...register("bio")} />
+								{errors.bio && <p className="mt-2 text-red-500">{errors.bio.message}</p>}
+							</Label>
+						</fieldset>
+
+						<h1>Links</h1>
+						<fieldset className="flex flex-col gap-4">
+							<Label className="relative flex-grow" htmlFor="bio">
+								<Globe className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+								<Input
+									placeholder="URL do github"
+									className="pl-10"
+									{...register("githubUrl")}
+								/>
+							</Label>
+							{errors.githubUrl && (
+								<p className="mt-2 text-red-500">{errors.githubUrl.message}</p>
+							)}
+						</fieldset>
+						<fieldset className="flex flex-col gap-4">
+							<Label className="relative flex-grow" htmlFor="bio">
+								<Globe className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+								<Input
+									placeholder="URL do linkedin"
+									className="pl-10"
+									{...register("linkedinUrl")}
+								/>
+							</Label>
+							{errors.linkedinUrl && (
+								<p className="mt-2 text-red-500">{errors.linkedinUrl.message}</p>
+							)}
+						</fieldset>
+						<fieldset className="flex flex-col gap-4">
+							<Label className="relative flex-grow" htmlFor="bio">
+								<Globe className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+								<Input
+									placeholder="URL da twitch"
+									className="pl-10"
+									{...register("twitchUrl")}
+								/>
+							</Label>
+							{errors.twitchUrl && (
+								<p className="mt-2 text-red-500">{errors.twitchUrl.message}</p>
+							)}
+						</fieldset>
+						<fieldset className="flex flex-col gap-4">
+							<Label className="relative flex-grow" htmlFor="bio">
+								<Globe className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+								<Input
+									placeholder="URL do portfolio"
+									className="pl-10"
+									{...register("websiteUrl")}
+								/>
+							</Label>
+							{errors.websiteUrl && (
+								<p className="mt-2 text-red-500">{errors.websiteUrl.message}</p>
+							)}
+						</fieldset>
+						<fieldset className="flex flex-col gap-4">
+							<Label className="relative flex-grow" htmlFor="bio">
+								<Globe className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+								<Input
+									placeholder="URL do youtube"
+									className="pl-10"
+									{...register("youtubeUrl")}
+								/>
+							</Label>
+							{errors.youtubeUrl && (
+								<p className="mt-2 text-red-500">{errors.youtubeUrl.message}</p>
+							)}
+						</fieldset>
+					</div>
+				</div>
+			</form>
+		</main>
+	)
+}
--- app/features/profile/pages/profile/components/profile-header.tsx
+++ app/features/profile/pages/profile/components/profile-header.tsx
@@ -5,13 +5,16 @@ import {
 	Edit2,
 	Github,
 	Globe,
+	Grid,
 	Linkedin,
 	Twitch,
 	Youtube,
 	type LucideIcon
 } from "lucide-react"
 
+import { useProfile } from "@/features/profile/hooks/use-profile"
 import { cn } from "@/libs/utils"
+import { Avatar, AvatarFallback, AvatarImage } from "@/shared/components/ui/avatar"
 import { Button, buttonVariants } from "@/shared/components/ui/button"
 import { useUser } from "@/shared/hooks/use-user"
 
@@ -24,28 +27,56 @@ export const socialIcons: Record<SocialPlatform, LucideIcon> = {
 	website: Globe
 }
 
-interface SocialLink {
-	id: string
-	type: SocialPlatform
-	url: string
-}
-
-interface ProfileHeaderProps {
-	bio: string
-	links?: SocialLink[]
-}
-
-export const ProfileHeader = ({ bio, links }: ProfileHeaderProps) => {
+export const ProfileHeader = () => {
 	const { user } = useUser()
+	const { profile } = useProfile()
+
+	const profileAvatarUrl = profile?.images?.find(
+		(image) => image.type === "profile-avatar"
+	)?.url
+	const profileCoverUrl = profile?.images?.find(
+		(image) => image.type === "profile-cover"
+	)?.url
 
 	return (
 		<div className="overflow-hidden rounded-lg border-y bg-card shadow">
 			<div className="relative h-48">
-				<img
-					src="https://cdn.discordapp.com/attachments/417530820450844675/1323054703227174932/zap.png?ex=67731e36&is=6771ccb6&hm=8225da6e8ca943ae2af7ae72b559019b18ba055febe5ca91a4ac3d9051a372f3&"
-					alt="User banner"
-					className="h-full w-full object-cover"
-				/>
+				{profileCoverUrl ? (
+					<img
+						src={profileCoverUrl}
+						alt="User banner"
+						className="h-full w-full object-cover"
+					/>
+				) : (
+					<div className="flex h-full w-full flex-wrap items-center justify-center bg-muted">
+						<div className="grid-cols-banner grid gap-1">
+							{[...Array(95).keys()].map((_, index: number) => {
+								const activeIndices = [
+									//D
+									0, 19, 38, 57, 76, 1, 21, 40, 59, 77,
+									//R
+									80, 61, 42, 23, 4, 5, 6, 42, 25, 44, 62, 82, 43,
+									//A
+									84, 65, 46, 27, 9, 29, 47, 48, 67, 86,
+									//F
+									88, 69, 50, 31, 12, 13, 14, 51, 52,
+									//T
+									93, 74, 55, 36, 16, 17, 18
+								]
+
+								return (
+									<div key={index} className="flex h-6 w-6 items-center justify-center">
+										{activeIndices.includes(index) ? (
+											<Grid className="h-6 w-6 text-primary" />
+										) : (
+											<span className="h-6 w-6"></span>
+										)}
+									</div>
+								)
+							})}
+						</div>
+					</div>
+				)}
 
 				<Link
 					to="/profile/edit"
@@ -63,11 +94,13 @@ export const ProfileHeader = ({ bio, links }: ProfileHeaderProps) => {
 				<div className="flex items-center justify-between">
 					<div className="flex items-center space-x-4">
 						<div className="relative -mt-16 flex h-24 w-24 items-center justify-center rounded-full bg-card">
-							<img
-								className="h-24 w-24 overflow-hidden rounded-full border-4 border-card"
-								src="https://avatars.githubusercontent.com/u/68834970?v=4"
-								alt="zap"
-							/>
+							<Avatar className="h-24 w-24 overflow-hidden rounded-full border-4 border-card">
+								<AvatarImage
+									src={profileAvatarUrl}
+									alt={`foto de perfil do usuário ${user?.name}`}
+								/>
+								<AvatarFallback>{user?.name.substring(0, 2)}</AvatarFallback>
+							</Avatar>
 							<Button
 								variant="secondary"
 								size="icon"
@@ -77,13 +110,15 @@ export const ProfileHeader = ({ bio, links }: ProfileHeaderProps) => {
 						</div>
 						<div>
 							<h1 className="text-2xl font-bold">{user?.name}</h1>
-							<p className="max-w-96 truncate text-sm text-muted-foreground">{bio}</p>
+							<p className="max-w-96 truncate text-sm text-muted-foreground">
+								{profile?.bio ?? "Este usuário ainda não adicionou uma biografia."}
+							</p>
 						</div>
 					</div>
 
 					<div className="flex items-center gap-2">
-						{links
-							? links
+						{profile
+							? profile.links
 									.filter(({ url }) => url)
 									.map(({ id, type, url }) => {
 										const Icon = socialIcons[type]
