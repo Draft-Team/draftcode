27bde21acff4aa7bb0662fe6c32d56a989e00c6f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-01-21 17:11:51 -0300

    remove unused type

--- app/features/challenges/components/render-form-blocks.tsx
+++ app/features/challenges/components/render-form-blocks.tsx
@@ -1,31 +1,21 @@
+import type { DBTypes } from "@/server/db/db-types"
+
 import { FigmaIframeFormBlock } from "./form-blocks/figma-iframe-form-block"
 import { TextFormBlock } from "./form-blocks/text-form-block"
 
-type Block =
-	| {
-			id: string
-			type: "text"
-			content: {
-				text: string
-			}
-	  }
-	| {
-			id: string
-			type: "figma"
-			content: {
-				url: string
-			}
-	  }
-
-export const RenderFormBlocks = ({ blocks }: { blocks: Block[] }) => {
+export const RenderFormBlocks = ({
+	blocks
+}: {
+	blocks: DBTypes["challengesTable"]["blocks"]
+}) => {
 	return (
 		<>
 			{blocks.map((block, index) => {
 				switch (block.type) {
 					case "text":
-						return <TextFormBlock index={index} key={block.id} />
+						return <TextFormBlock index={index} key={Math.random() + index} />
 					case "figma":
-						return <FigmaIframeFormBlock index={index} key={block.id} />
+						return <FigmaIframeFormBlock index={index} key={Math.random() + index} />
 				}
 			})}
 		</>
