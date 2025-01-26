8ffc2947d210271d781daf67f6e1d52d4d305c89
Vitor <68834970+vitorFRE@users.noreply.github.com>
2025-01-25 20:57:14 -0400

    Feat/rankings page (#31)
    * feat: add initial page
    
    * feat: use real data in rankings
    
    * refactor to compare with the user email
    
    * feat: add rankings at nav links
    
    ---------
    
    Co-authored-by: Matheus Pergoli <matheus.pergoli2015@yahoo.com>

---
+++ app/features/ranks/pages/rankings-page.tsx
@@ -0,0 +1,86 @@
+import { useSuspenseQuery } from "@tanstack/react-query"
+import { Star } from "lucide-react"
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/shared/components/ui/card"
+import { HeroSection } from "@/shared/components/ui/hero-section"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/components/ui/tabs"
+import { useUser } from "@/shared/hooks/use-user"
+
+import { Leaderboard } from "../components/leaderboard"
+import { columns } from "../components/table/columns"
+import { DataTable } from "../components/table/data-table"
+import { rankingQueryOption } from "../queries"
+
+export const RankingsPage = () => {
+	const { data: rankings } = useSuspenseQuery(rankingQueryOption)
+	const { user } = useUser()
+
+	const getRowClassName = (row: { email: string }) => {
+		return row.email === user?.email ? "bg-primary/10" : ""
+	}
+
+	return (
+		<main>
+			<HeroSection>
+				<div className="container">
+					<h2 className="text-4xl font-bold">Ranking de Usuários</h2>
+					<p className="text-muted-foreground">
+						Veja quem está no topo! Acumule pontos concluindo desafios.
+					</p>
+				</div>
+			</HeroSection>
+
+			<section className="container mt-6 flex flex-col gap-4">
+				<h1 className="text-3xl font-bold">Destaques</h1>
+				<Tabs defaultValue="geral" className="space-y-4">
+					<TabsList>
+						<TabsTrigger value="geral">Pontos</TabsTrigger>
+					</TabsList>
+					<TabsContent value="geral" className="space-y-4">
+						<Leaderboard title="Pontos" entries={rankings} />
+					</TabsContent>
+				</Tabs>
+			</section>
+
+			<section className="container mt-6 space-y-4">
+				<h1 className="text-3xl font-bold">Ranking</h1>
+				<DataTable columns={columns} data={rankings} getRowClassName={getRowClassName} />
+			</section>
+
+			<section className="container space-y-4">
+				<h1 className="text-3xl font-bold">Como Ganhar Pontos?</h1>
+				<Card className="rounded-[2px] border bg-secondary">
+					<CardHeader>
+						<CardTitle className="text-xl">Concluir Desafios</CardTitle>
+					</CardHeader>
+					<CardContent className="space-y-4">
+						<ul className="space-y-2">
+							<li className="flex items-center gap-2">
+								<Star className="h-4 w-4 text-yellow-500" />
+								<span>Fácil: Pontos variáveis (definidos pelo criador do desafio)</span>
+							</li>
+							<li className="flex items-center gap-2">
+								<Star className="h-4 w-4 text-yellow-500" />
+								<span>
+									Intermediário: Pontos variáveis (definidos pelo criador do desafio)
+								</span>
+							</li>
+							<li className="flex items-center gap-2">
+								<Star className="h-4 w-4 text-yellow-500" />
+								<span>
+									Avançado: Pontos variáveis (definidos pelo criador do desafio)
+								</span>
+							</li>
+							<li className="flex items-center gap-2">
+								<Star className="h-4 w-4 text-yellow-500" />
+								<span>
+									Especialista: Pontos variáveis (definidos pelo criador do desafio)
+								</span>
+							</li>
+						</ul>
+					</CardContent>
+				</Card>
+			</section>
+		</main>
+	)
+}
---
+++ app/features/ranks/services/get-points-ranking.ts
@@ -0,0 +1,37 @@
+import { createServerFn } from "@tanstack/start"
+import { and, desc, eq, sql } from "drizzle-orm"
+
+import { db } from "@/server/db/client"
+import {
+	imagesEntityTable,
+	imagesTable,
+	profilesTable,
+	usersTable
+} from "@/server/db/schema"
+
+export const $getPointsRanking = createServerFn().handler(async () => {
+	const rankings = await db
+		.select({
+			name: usersTable.name,
+			totalExperience: profilesTable.totalExperience,
+			email: usersTable.email,
+			imageUrl: imagesTable.url,
+			rank: sql<number>`ROW_NUMBER() OVER (ORDER BY ${profilesTable.totalExperience} DESC)`.as(
+				"rank"
+			)
+		})
+		.from(usersTable)
+		.leftJoin(profilesTable, eq(usersTable.id, profilesTable.userId))
+		.leftJoin(
+			imagesEntityTable,
+			and(
+				eq(profilesTable.id, imagesEntityTable.entityId),
+				eq(imagesEntityTable.entityType, "profile")
+			)
+		)
+		.leftJoin(imagesTable, eq(imagesEntityTable.imageId, imagesTable.id))
+		.orderBy(desc(profilesTable.totalExperience))
+		.all()
+
+	return rankings
+})
