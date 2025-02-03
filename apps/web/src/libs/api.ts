40aaeffd003e9de4ac073745dc7e47c537af7d0f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-02-03 02:27:43 -0300

    fix api to throw errors and upload routes

--- apps/web/src/libs/api.ts
+++ apps/web/src/libs/api.ts
@@ -2,11 +2,40 @@ import { hc } from "hono/client"
 import type { ApiRoutes } from "@draftcode/types"
 import { env } from "@/environment/env"
 
-export const api = hc<ApiRoutes>(env.VITE_BACKEND_URL, {
-	fetch: (input: RequestInfo | URL, init?: RequestInit) => {
-		return fetch(input, {
-			...init,
-			credentials: "include"
-		})
+interface ErrorResponse {
+	error: string
+	success: false
+}
+
+class ApiError extends Error {
+	constructor(
+		message: string,
+		public status: number,
+		public data?: ErrorResponse
+	) {
+		super(message)
+		this.name = "ApiError"
 	}
+}
+
+const customFetch = async (input: RequestInfo | URL, init?: RequestInit) => {
+	const response = await fetch(input, {
+		...init,
+		credentials: "include"
+	})
+
+	if (!response.ok) {
+		const data = (await response.json()) as ErrorResponse
+		throw new ApiError(
+			data.error || "Ocorreu um erro na requisição",
+			response.status,
+			data
+		)
+	}
+
+	return response
+}
+
+export const api = hc<ApiRoutes>(env.VITE_BACKEND_URL, {
+	fetch: customFetch
 }).api
