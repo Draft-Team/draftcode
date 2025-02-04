084884b37b7e338c14a440ad7cb98c04982721d1
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-02-04 11:20:03 -0300

    add missing configs

--- packages/utils/src/get-base-url.ts
+++ packages/utils/src/get-base-url.ts
@@ -1,5 +1,6 @@
 export const getBaseUrl = () => {
 	if (typeof window !== "undefined") return window.location.origin
 	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+	if (process.env.RENDER_EXTERNAL_URL) return process.env.RENDER_EXTERNAL_URL
 	return `http://localhost:${process.env.PORT ?? 3000}`
 }
