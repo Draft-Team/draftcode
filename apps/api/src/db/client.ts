62ff2fb4e54aa224a6d47f2bf6d6dd9d215e707b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-02-03 23:41:14 -0300

    add config

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -8,9 +8,12 @@ const globalForDb = globalThis as unknown as {
 	client: Client | undefined
 }
 
-const url = env.NODE_ENV === "production" ? env.DATABASE_URL : env.DATABASE_URL_DEV
+const config = {
+	authToken: env.DATABASE_AUTH_TOKEN,
+	url: env.NODE_ENV === "production" ? env.DATABASE_URL : env.DATABASE_URL_DEV
+} satisfies { url: string; authToken: string }
 
-export const client = globalForDb.client ?? createClient({ url })
+export const client = globalForDb.client ?? createClient(config)
 
 if (env.NODE_ENV === "production") globalForDb.client = client
 
