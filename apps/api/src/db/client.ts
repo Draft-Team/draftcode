f21bafdb1befcbfd9356c0df0f6ecd91d07160ec
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-02-03 23:43:41 -0300

    add config

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -8,14 +8,16 @@ const globalForDb = globalThis as unknown as {
 	client: Client | undefined
 }
 
+const isProd = env.NODE_ENV === "production"
+
 const config = {
-	authToken: env.DATABASE_AUTH_TOKEN,
-	url: env.NODE_ENV === "production" ? env.DATABASE_URL : env.DATABASE_URL_DEV
-} satisfies { url: string; authToken: string }
+	url: isProd ? env.DATABASE_URL : env.DATABASE_URL_DEV,
+	authToken: isProd ? env.DATABASE_AUTH_TOKEN : undefined
+} satisfies { url: string; authToken?: string }
 
 export const client = globalForDb.client ?? createClient(config)
 
-if (env.NODE_ENV === "production") globalForDb.client = client
+if (isProd) globalForDb.client = client
 
 export const db = drizzle(client, {
 	schema
